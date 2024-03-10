960ef9f56b4ca2e5afbdf678c2903e55daa3a863
Karan <karankpatil2016@gmail.com>
2024-03-10 12:55:28 +0530

    remove Untitled-1.html

---
+++ BitManipulation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class BitManipulation{
+    public static void main(String[] args) {
+        int n = 5;
+        int pos = 3;
+        int bitMask = 1 << pos;
+
+
+        int newNumber = bitMask | n;
+        System.out.println(newNumber);
+        
+        // if ((bitMask & n) == 0) {
+        // System.out.println("bit was zero");
+
+        // }else{
+
+        // System.out.println("bit was one");
+        // }
+    }
+}
